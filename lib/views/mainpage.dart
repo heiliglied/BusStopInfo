9818a604c96c55621b7b9e7c7f3ae132a60adbfd
힘내요 괜찮아요 얼마나 쓸쓸해요 <loriluri@hotmail.com>
2021-11-28 12:12:32 +0900

    initial commit

---
+++ lib/views/mainpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body:Center(
+          child: Text("메인페이지"),
+        )
+    );
+  }
+}

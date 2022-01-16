157d0318442d0faacb22e6cebae7efd488beb3a7
힘내요 괜찮아요 얼마나 쓸쓸해요 <loriluri@hotmail.com>
2022-01-16 15:59:09 +0900

    메인페이지 UI 작성 테스트. webview 라이브러리 추가. 메니페스트 및 plist 설정 추가.

--- lib/views/mainpage.dart
+++ lib/views/mainpage.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:Center(
-          child: Text("메인페이지"),
+        body:Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.60,
+              child: WebView(
+                initialUrl: "https://m.map.naver.com/",
+                javascriptMode: JavascriptMode.unrestricted,
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.40,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                margin: const EdgeInsets.all(5.0),
+                color: Colors.blue,
+              ),
+            )
+          ],
         )
     );
   }

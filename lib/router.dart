47c73825270f98e23a4d5731773427615c0d6949
wooyj <glem1004@gmail.com>
2022-01-16 15:57:40 +0900

    webview 설정, listview 영역 남겨놓음

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,13 @@
+import 'package:busstopinfo/views/mainpage.dart';
 import 'package:flutter/widgets.dart';
 
 class Routes {
   Routes._();
   static const String mainPage = "/";
-  static const String splashPage = "/splash";
+  // static const String splashPage = "/splash";
 
   static final routes = <String, WidgetBuilder>{
-    mainPage: (BuildContext context) => mainPage(),
-    splashPage: (BuildContext context) => splashPage(),
+    mainPage: (BuildContext context) => MainPage(),
+    // splashPage: (BuildContext context) => splashPage(),
   };
 }
--- lib/views/mainpage.dart
+++ lib/views/mainpage.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:Center(
-          child: Text("메인페이지"),
-        )
+      body: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.6,
+            child: const WebView(
+              initialUrl: "https://flutter.dev",
+              // javascriptMode: JavascriptMode.unrestricted,
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.4,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              margin: const EdgeInsets.all(5),
+              color: Colors.blue,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
+
+

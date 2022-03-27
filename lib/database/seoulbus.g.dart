0b7fa2708179833e373986098a116d72eb020a9e
힘내요 괜찮아요 얼마나 쓸쓸해요 <loriluri@hotmail.com>
2022-03-27 11:53:43 +0900

    데이터베이스 설정 추가, 메뉴 view 파일 생성

---
+++ lib/database/seoulbus.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'seoulbus.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$SeoulBusDaoMixin on DatabaseAccessor<MyDatabase> {
+  $SeoulBusTable get seoulBus => attachedDatabase.seoulBus;
+}

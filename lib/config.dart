9f3fe3bf4d4104298e3b5e09e7b27957dd7fa883
힘내요 괜찮아요 얼마나 쓸쓸해요 <loriluri@hotmail.com>
2022-04-24 12:32:31 +0900

    RichText, const import 확인.

---
+++ lib/config.dart
@@ -0,0 +1,1 @@
+const String seoulBusLink = 'https://data.seoul.go.kr/dataList/OA-21231/S/1/datasetView.do';
--- lib/views/menus/upload.dart
+++ lib/views/menus/upload.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 import 'package:file_picker/file_picker.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 import 'package:busstopinfo/views/partitions/BaseAppBar.dart';
 import 'package:busstopinfo/views/partitions/BaseDrawer.dart';
+
+import 'package:busstopinfo/config.dart' as configs;
 
 class UploadPage extends StatefulWidget {
   UploadPage({Key? key}) : super(key: key);
@@ -16,9 +19,39 @@ class _UploadPage extends State<UploadPage> {
     Map arguments = ModalRoute.of(context)?.settings.arguments as Map;
     return Scaffold(
         appBar: BaseAppBar(arguments['title']),
+        //appBar: BaseAppBar(configs.seoulBusLink),
         endDrawer: BaseDrawer(),
         body: Column(
-
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 30,
+              child: RichText(
+                text: TextSpan(
+                  text: '서울시 버스정류장 정보 : ',
+                  style: TextStyle(fontSize: 24, color: Colors.black),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '바로가기',
+                      recognizer: TapGestureRecognizer() ..onTap = () async {
+                        //링크 실행 함수 추가.
+                        //await launch(configs.seoulBusLink, forceWebView: true, forceSafariVC: true);
+                        await launchUrl(Uri.parse(configs.seoulBusLink));
+                      },
+                      style: TextStyle(color: Colors.blue,)
+                    )
+                  ]
+                ),
+              )
+            ),
+          ],
+          /*
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 30,
+            child:
+          ),
+           */
         )
     );
   }
